011ec6eb8747e4f7bc4afc9b905d3e78428aa378
Tean Concio <tean_concio@dlsu.edu.ph>
2025-02-01 01:48:09 +0800

    Started implementing it. Failed

--- STDISCM PS 1 - Threaded Prime Number Search/PrimeSearch.cs
+++ STDISCM PS 1 - Threaded Prime Number Search/PrimeSearch.cs
@@ -28,15 +28,19 @@ namespace STDISCM_PS_1___Threaded_Prime_Number_Search
         public static PrintMode PrintMode { get; set; }
         public static ThreadTaskDivisionMode ThreadTaskDivisionMode { get; set; }
 
-        public static PrimeSearchThread[] Threads { get; set; }
+        private static PrimeSearchThread[] Threads;
         public static long StartMilliTime { get; set; }
-        public static SortedSet<int> PrimesList { get; set; } = new SortedSet<int>([2, 3, 5, 7]);   // Memoization
-        private static readonly object PrimesListLock = new object();
 
-        private static int CurrentDivisorChecked { get; set; } = 2;       // For By Divisibility Task Division
+        private static int CurrentPrimeListUsed = 1;       // For seamless prime list update for threading
+        private static SortedSet<int> PrimesList1 = new SortedSet<int>([2, 3, 5, 7]);   // Memoization
+        private static readonly ReaderWriterLockSlim PrimesListLock1 = new ReaderWriterLockSlim();
+        private static SortedSet<int> PrimesList2 = new SortedSet<int>();   // For seamless prime list update for threading
+        private static readonly ReaderWriterLockSlim PrimesListLock2 = new ReaderWriterLockSlim();
+
+        private static int CurrentDivisorChecked = 2;       // For By Divisibility Task Division
         private static readonly object DivisorCheckLock = new object();
 
-        private static int CurrentNumberChecked { get; set; } = 1;       // For By Number Task Division
+        private static int CurrentNumberChecked = 1;       // For By Number Task Division
         private static readonly object NumberCheckLock = new object();
 
 
@@ -207,19 +211,22 @@ namespace STDISCM_PS_1___Threaded_Prime_Number_Search
                 thread.Join();
             }
 
+            // Set current prime list to the prime list with more primes
+            CurrentPrimeListUsed = PrimesList1.Count > PrimesList2.Count ? 1 : 2;
+
             // If Print Mode is Delayed
             if (PrintMode == PrintMode.DELAYED)
             {
                 // Get Current Time in Microseconds
                 long microtimeNow = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-                foreach (int prime in PrimesList)
+                foreach (int prime in GetPrimeList())
                 {
                     Console.WriteLine($"Main Thread [{microtimeNow - StartMilliTime} ms]: {prime} is prime");
                 }
             }
 
             Console.WriteLine();
-            Console.WriteLine($"Total Primes Found: {PrimesList.Count}");
+            Console.WriteLine($"Total Primes Found: {GetPrimeList().Count}");
         }
 
         // Check if a number is prime
@@ -233,13 +240,20 @@ namespace STDISCM_PS_1___Threaded_Prime_Number_Search
             // Check if divisible by numbers in the prime list up to the square root of the number
             int lastPrimeChecked = 0;
 
-            for (int i = 0; i < PrimesList.Count; i++)
+            for (int i = 0; i < GetPrimeList().Count; i++)
             {
                 int currentPrime;
-                lock (PrimesListLock)
-                {
-                    currentPrime = PrimesList.ElementAt(i);
-                }
+
+                //try { 
+                //    currentPrime = GetPrimeList().ElementAt(i); 
+                //}
+                //catch (Exception e)
+                //{
+                //    Console.WriteLine(e.Message);
+                //    return false;
+                //}
+
+                currentPrime = GetPrimeList().ElementAt(i);
 
                 // If the square of the current prime is greater than the number
                 // Or the difference between the current prime and previous prime is greater than PrimeRange / (NumThreads * NumThreads) (To prevent skipping primes by range division)
@@ -282,9 +296,61 @@ namespace STDISCM_PS_1___Threaded_Prime_Number_Search
         // Add prime to the sorted set
         public static void AddPrime(int prime)
         {
-            lock (PrimesListLock)
+            // Write lock both prime lists
+            PrimesListLock1.EnterWriteLock();
+            PrimesListLock2.EnterWriteLock();
+            try
             {
-                PrimesList.Add(prime);
+                // Add prime to the unused prime list
+                switch (CurrentPrimeListUsed)
+                {
+                    case 1:
+                        PrimesList2.Add(prime);
+                        break;
+                    case 2:
+                        PrimesList1.Add(prime);
+                        break;
+                    default:
+                        break;
+                }
+
+                // If the prime list count difference is greater than PrimeRange / NumThreads
+                if (Math.Abs(PrimesList1.Count - PrimesList2.Count) > 5)
+                {
+                    // Update the prime list not being used and switch to the updated prime list
+                    switch (CurrentPrimeListUsed)
+                    {
+                        case 1:
+                            PrimesList1.UnionWith(PrimesList2);
+                            CurrentPrimeListUsed = 2;
+                            break;
+                        case 2:
+                            PrimesList2.UnionWith(PrimesList1);
+                            CurrentPrimeListUsed = 1;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            finally
+            {
+                PrimesListLock1.ExitWriteLock();
+                PrimesListLock2.ExitWriteLock();
+            }
+        }
+
+        // Get prime list being used
+        public static SortedSet<int> GetPrimeList()
+        {
+            switch (CurrentPrimeListUsed)
+            {
+                case 1:
+                    return PrimesList1;
+                case 2:
+                    return PrimesList2;
+                default:
+                    return PrimesList1;
             }
         }
 
